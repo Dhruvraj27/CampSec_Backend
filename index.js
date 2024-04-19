c1de6b1157ae541abdf75b0167fae9eabf8da1a5
Dhruvraj27 <73035080+Dhruvraj27@users.noreply.github.com>
2024-04-20 01:11:03 +0530

    changes in the cron schedule time format issue

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const getEntryTime = () => {
   let year = new Date().getFullYear();
   let day = new Date().getDate();
   let month = new Date().getMonth();
-  let hours = new Date().getHours();
+  let Aslihours = new Date().getHours();
+  let hours = new Date().getHours() % 12 || 12;
   let Minutes = new Date().getMinutes();
   let seconds = new Date().getSeconds();
   let getDate = `${day}-${month}-${year}`;
-  let getCurrentTime = `${hours}:${Minutes}:${seconds} ${
-    hours < 12 ? "AM" : "PM"
-  }`;
+  let getCurrentTime = `${hours}:${Minutes}:${seconds} ${Aslihours < 12 ? "AM" : "PM"
+    }`;
   return `${getDate} ${getCurrentTime}`;
 };
 
